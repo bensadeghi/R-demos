e4fddc52becf15ad71859b075bc92ac0ea1c7566
Ben Sadeghi <ben.sadeghi1@gmail.com>
2016-07-03 13:13:24 +0800

    update AirOnTime example

--- MS-R/RxHadoop_RxSpark_Logit_AirOnTime.R
+++ MS-R/RxHadoop_RxSpark_Logit_AirOnTime.R
@@ -1,11 +1,17 @@
-### RxSpark Context - build logistic regression model using AirOnTime data
-### Run this script on an HDInsight Premium cluster
+### RxHadoop & RxSpark Contexts
+### Build logistic regression model using AirOnTime data
 
-# Set the HDFS (WASB) location of example data
+########## Fetch Data ##########
+# Define the HDFS file system
+hdfsFS <- RxHdfsFileSystem()
+# Set the HDFS location of example data
 bigDataDirRoot <- "/example/data"
 # create a local folder for storaging data temporarily
 source <- "/tmp/AirOnTimeCSV2012"
 dir.create(source)
+# Set directory in bigDataDirRoot to load the data into
+inputDir <- file.path(bigDataDirRoot, "AirOnTimeCSV2012")
+
 # Download data to the tmp folder
 remoteDir <- "http://packages.revolutionanalytics.com/datasets/AirOnTimeCSV2012"
 download.file(file.path(remoteDir, "airOT201201.csv"), file.path(source, "airOT201201.csv"))
@@ -20,8 +26,7 @@ download.file(file.path(remoteDir, "airOT201209.csv"), file.path(source, "airOT2
 download.file(file.path(remoteDir, "airOT201210.csv"), file.path(source, "airOT201210.csv"))
 download.file(file.path(remoteDir, "airOT201211.csv"), file.path(source, "airOT201211.csv"))
 download.file(file.path(remoteDir, "airOT201212.csv"), file.path(source, "airOT201212.csv"))
-# Set directory in bigDataDirRoot to load the data into
-inputDir <- file.path(bigDataDirRoot,"AirOnTimeCSV2012") 
+
 # Make the directory
 rxHadoopMakeDir(inputDir)
 # Copy the data from source to input
@@ -29,76 +34,89 @@ rxHadoopCopyFromLocal(source, bigDataDirRoot)
 # List files copied onto Hadoop
 rxHadoopListFiles(inputDir)
 
-# Define the HDFS (WASB) file system
-hdfsFS <- RxHdfsFileSystem()
+
+########## Format Data ##########
 # Create info list for the airline data
 airlineColInfo <- list(
     DAY_OF_WEEK = list(type = "factor"),
-    ORIGIN = list(type = "factor"),
-    DEST = list(type = "factor"),
+    DISTANCE = list(type = "integer"),
     DEP_TIME = list(type = "integer"),
     ARR_DEL15 = list(type = "logical"))
 
 # Get all the column names
 varNames <- names(airlineColInfo)
 
-# Formula to use
-formula = "ARR_DEL15 ~ ORIGIN + DAY_OF_WEEK + DEP_TIME + DEST"
 
-# Define the text data source in hdfs
+####### Set Hadoop Compute Context #######
+myHadoopCluster <- RxHadoopMR(consoleOutput=TRUE)
+# Set the compute context
+rxSetComputeContext(myHadoopCluster)
+# Define the HDFS file system
+hdfsFS <- RxHdfsFileSystem()
+
+# Define the text data source in HDFS
 airOnTimeData <- RxTextData(inputDir, colInfo = airlineColInfo, varsToKeep = varNames, fileSystem = hdfsFS)
-# Define the text data source in local system
-airOnTimeDataLocal <- RxTextData(source, colInfo = airlineColInfo, varsToKeep = varNames)
 
-# Take a look at the data
-head(airOnTimeDataLocal, 1)
+# Define XDF file and import from CSV data in HDFS
+filename <- file.path(bigDataDirRoot, "AirOnTimeData")
+airOnTimeDataXdf <- RxXdfData(filename, fileSystem = hdfsFS )
+rxImport(inData = airOnTimeData, outFile = airOnTimeDataXdf,
+         rowsPerRead = 250000, overwrite = TRUE, numRows = -1)
 
 # Compute summary information
-adsSummary <- rxSummary(~ARR_DEL15 + DEP_TIME + DAY_OF_WEEK, data = airOnTimeDataLocal)
+adsSummary <- rxSummary(~ARR_DEL15 + DISTANCE + DEP_TIME + DAY_OF_WEEK, data = airOnTimeDataXdf)
 adsSummary
 
 
-########## LOCAL ##########
-# Set a local compute context
-rxSetComputeContext("local")
+########## Build Logistic Regression Model with Hadoop ##########
+# Formula to use
+formula <- "ARR_DEL15 ~ DISTANCE + DAY_OF_WEEK + DEP_TIME"
+
 # Run a logistic regression
-system.time(
-    modelLocal <- rxLogit(formula, data = airOnTimeDataLocal)
-)
-# Display a summary 
-summary(modelLocal)
+modelHadoop <- rxLogit(formula, data = airOnTimeDataXdf)
 
-
-########## HADOOP ##########
-# Define the Hadoop compute context 
-myHadoopCluster <- RxHadoopMR(consoleOutput=TRUE)
-# Set the compute context 
-rxSetComputeContext(myHadoopCluster)
-# Run a logistic regression 
-system.time(  
-    modelHadoop <- rxLogit(formula, data = airOnTimeData)
-)
-# Display a summary
+# Display model summary
 summary(modelHadoop)
 
+
+########## Make Predictions with Hadoop ##########
 # Make predictions using the logit model
-hdfsFS <- RxHdfsFileSystem(hostName="default", port=0)
-filename <- file.path(bigDataDirRoot, "predictions")
-output <- RxXdfData(filename, fileSystem=hdfsFS, createCompositeSet=TRUE)
-predictions <- rxPredict(modelObject=modelHadoop, data=airOnTimeData, outData=output,
-			computeResiduals=TRUE, overwrite=TRUE)
+filename <- file.path(bigDataDirRoot, "AirOnTimePredictions")
+outputXdf <- RxXdfData(filename, fileSystem = hdfsFS, createCompositeSet = TRUE)
+predictions <- rxPredict(modelObject = modelHadoop, data = airOnTimeDataXdf,
+			writeModelVars = TRUE, predVarNames = "Score",
+			outData = outputXdf, overwrite = TRUE)
+
 # Print results
-rxGetInfo(predictions, getVarInfo=TRUE, numRows=5)
+rxGetInfo(predictions, getVarInfo = TRUE, numRows = 5)
 
 
-########## SPARK ##########
+#####################################################
+########## Change Compute Context to SPARK ##########
 # Define the Spark compute context 
 mySparkCluster <- RxSpark(consoleOutput=TRUE)
 # Set the compute context 
 rxSetComputeContext(mySparkCluster)
-# Run a logistic regression 
-system.time(  
-    modelSpark <- rxLogit(formula, data = airOnTimeData)
-)
-# Display a summary
+
+
+########## Build Model with SPARK ##########
+# Formula to use
+formula <- "ARR_DEL15 ~ DISTANCE + DAY_OF_WEEK + DEP_TIME"
+
+# Run a logistic regression
+modelSpark <- rxLogit(formula, data = airOnTimeDataXdf)
+
+# Display model summary
 summary(modelSpark)
+
+
+########## Make Predictions with Spark ##########
+# Make predictions using the logit model
+filename <- file.path(bigDataDirRoot, "AirOnTimePredictions")
+outputXdf <- RxXdfData(filename, fileSystem = hdfsFS, createCompositeSet = TRUE)
+predictions <- rxPredict(modelObject = modelSpark, data = airOnTimeDataXdf,
+			writeModelVars = TRUE, predVarNames = "Score",
+			outData = outputXdf, overwrite = TRUE)
+
+# Print results
+rxGetInfo(predictions, getVarInfo = TRUE, numRows = 5)
